9d9ef815cf3d92df0f8edbc934f66a12ca2a3e0e
SolalDR <solal.dussout-revel@hotmail.fr>
2017-03-27 23:35:50 +0200

    Fixed bug association User-ArtTag

--- app/models/art_tag.rb
+++ app/models/art_tag.rb
@@ -1,4 +1,7 @@
 class ArtTag < ApplicationRecord
   has_many :exhibitions_parts
   has_many :exhibitions, :through => :exhibitions_parts
+
+  has_many :users_tag_parts
+  has_many :users, :through => :users_tag_parts
 end
