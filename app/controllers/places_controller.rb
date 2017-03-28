411ad925a1e8986112bc8a441c91caa9083bfb59
Bastien Robert <bastienrbt@protonmail.com>
2017-03-28 15:51:58 +0200

    Fixed bug before action in Place_controller

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,4 +1,5 @@
 class PlacesController < ApplicationController
+  before_action :set_place, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
 
   # GET /places
