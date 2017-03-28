0325ab64ae25a0329a53cd196e801d7ac7b22e9f
Bastien Robert <bastienrbt@protonmail.com>
2017-03-28 16:12:57 +0200

    Comment some unused perms for the moment

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,15 +1,15 @@
 class Ability
-  include CanCan::Ability
-
-  def initialize(user)
-    # user ||= User.new
-    #
-    # can [:read], Place
-
-
-    # if user.admin?     ## Any admin role ?
-    #   can :manage, :all
-    # end
-
-  end
+  # include CanCan::Ability
+  #
+  # def initialize(user)
+  #   # user ||= User.new
+  #   #
+  #   # can [:read], Place
+  #
+  #
+  #   # if user.admin?     ## Any admin role ?
+  #   #   can :manage, :all
+  #   # end
+  #
+  # end
 end
