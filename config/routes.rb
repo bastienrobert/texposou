e29fa773042d45e9d148a7b7cc28fca95537ba71
mharribey <mathieuharribey@hotmail.fr>
2017-03-28 14:26:36 +0200

    begin header

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,8 @@ Rails.application.routes.draw do
   get "exhibitions/:id/new", to: "exhibitions#new", as: :add_exhibition
   get "visits/:id/new", to: "visits#new", as: :add_visit
   get "participations/:id/new", to: "participations#new", as: :add_participation
+
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
 end
