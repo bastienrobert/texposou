c5adb3b3867f2a0ff67fea10af0d61c8f72a9983
SolalDR <solal.dussout-revel@hotmail.fr>
2017-03-29 12:40:19 +0200

    ArtTagManage

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,6 +16,117 @@
 //= require bootstrap-sprockets
 //= require_tree .
 
-document.addEventListener("turbolinks:load", function() {
 
+Node.prototype.remove = function(){
+  var parent = this.parentNode;
+  if(parent){
+    parent.removeChild(this);
+  }
+}
+
+Node.prototype.insertAfter = function(nouvelElement, refElement){
+  var that = this.nextSibling;
+  if(that){
+    that.insertBefore(nouvelElement, refElement);
+  } else {
+    this.appendChild(nouvelElement);
+  }
+}
+
+ArtTagManage = {
+  form: {},
+  tagContent:[],
+
+  initEventDetete:function(tag){
+    var deleteEl = tag.getElementsByClassName("art_tag_delete")[0];
+    var self = this;
+    deleteEl.addEventListener("click", function(){
+      tag.remove();
+      self.updateTagContent();
+      self.defineFocus();
+    }, false)
+  },
+
+  updateTagContent:function(){
+    this.tagContent = [];
+    for(i=0; i<this.tags.length; i++){
+      this.tagContent.push(this.tags[i].getAttribute("data-name"));
+    }
+    this.hiddenField.value = this.tagContent.join(", ");
+  },
+
+  formSubmitClick: function(){
+    var tagContent = this.form.text.value, test=true;
+    if(tagContent){
+      for(i=0; i<this.tags.length; i++){
+        test = this.tags[i].getAttribute("data-name") === tagContent ? false : true;
+      }
+      if(test){
+        this.addTag(tagContent);
+      }
+    }
+  },
+
+  initEvents:function(){
+    var self = this;
+    for(i=0; i<this.tags.length; i++){
+      this.initEventDetete(this.tags[i]);
+    }
+    this.form.submit.addEventListener("click", function(){
+      self.formSubmitClick();
+    }, false)
+
+    this.form.text.addEventListener("focus", function(){
+      window.addEventListener("keypress", self.listenerKeyPress, false)
+    }, false)
+    this.form.text.addEventListener("focusout", function(){
+      window.removeEventListener("keypress", self.listenerKeyPress)
+    }, false)
+  },
+
+  listenerKeyPress:function(e){
+    if(e.keyCode === 13){
+      ArtTagManage.formSubmitClick();
+      return false;
+    }
+    return true;
+  },
+
+  blockSubmit:function(e){
+    e.preventDefault();
+    return false;
+  },
+
+  defineFocus:function(){
+    this.form.text.value = "";
+    this.form.text.focus();
+  },
+
+  addTag:function(content){
+    var container = document.createElement("fragment");
+    var prototype = this.tagManageContainer.getAttribute("data-prototype");
+    prototype = prototype.replace(/__name__/g, content);
+    container.innerHTML = prototype;
+    var el = container.firstChild;
+    this.tagManageContainer.appendChild(el);
+    this.initEventDetete(el);
+    this.updateTagContent();
+    this.defineFocus();
+  },
+
+  init:function(){
+    this.tagManage = document.getElementById("art_tag_manage");
+    this.tagManageContainer = this.tagManage.getElementsByClassName("art_tags_container")[0];
+    this.tags = this.tagManage.getElementsByClassName("art_tag");
+    this.form.text = document.getElementById("add_tag_text");
+    this.form.submit = document.getElementById("add_tag_submit");
+    this.hiddenField = document.getElementById("tag-field");
+    this.formParent = document.getElementById("edit_user_1");
+    this.updateTagContent();
+    this.initEvents();
+  }
+}
+
+document.addEventListener("turbolinks:load", function() {
+  ArtTagManage.init();
 })
