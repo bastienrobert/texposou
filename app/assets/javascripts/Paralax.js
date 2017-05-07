7afadb865655cd6ed7f946d40d395eca583de903
SolalDR <solal.dussout-revel@hotmail.fr>
2017-05-07 14:37:44 +0200

    better paralax script

--- app/assets/javascripts/Paralax.js
+++ app/assets/javascripts/Paralax.js
@@ -9,7 +9,15 @@ function setPrefix(key,value){
 
 AwesomeParalax = {
   intensity: 300,
-  offset: 0,
+  unit:"px",
+  cssKey: "transform",
+  config : function(args){
+    if(args){
+      if(args.offset && !isNaN(args.offset)) this.offset = args.offset;
+      if(args.intensity && !isNaN(args.intensity)) this.intensity = args.intensity;
+      if(args.cssKey) this.cssKey = args.cssKey;
+    }
+  },
   updatePositions:function(){
     var that = AwesomeParalax;
     for(i=0; i<that.els.length; i++){
@@ -18,7 +26,8 @@ AwesomeParalax = {
   },
   calc:function(el){
     var ratio = (el.position.y+el.offsetHeight - this.top)/(window.innerHeight+el.offsetHeight);
-    el.setAttribute("style", setPrefix("transform", "translateY("+ratio*this.intensity+"px) translateZ(0)"));
+    var intensity = ratio*this.intensity*1;
+    el.setAttribute("style", setPrefix(this.cssKey, "translateY("+intensity+this.unit+") translateZ(0)"));
   },
   main:function(){
     var that = AwesomeParalax, el;
@@ -32,10 +41,7 @@ AwesomeParalax = {
   init:function(args){
     this.els = document.querySelectorAll("*[data-paralax]");
     if(this.els){
-      if(args){
-        if(args.offset && !isNaN(args.offset)) this.offset = args.offset;
-        if(args.intensity && !isNaN(args.intensity)) this.intensity = args.intensity;
-      }
+      this.config(args);
       this.horizon = window.scrollTop;
       this.updatePositions();
       this.main();
