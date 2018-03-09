b8d5cd22bd72876dcc2c923ec0cefd1330cbc1e5
Bastien <bastienrobert@outlook.com>
2018-03-09 23:47:25 +0100

    Edit the paperclip config to use the app w/ persistant storage

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,5 +1,5 @@
-if Rails.env.production?
-  Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
-  Paperclip::Attachment.default_options[:s3_host_name] = 's3-eu-central-1.amazonaws.com'
-  Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
-end
+# if Rails.env.production?
+#   Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
+#   Paperclip::Attachment.default_options[:s3_host_name] = 's3-eu-central-1.amazonaws.com'
+#   Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
+# end
